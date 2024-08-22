1919eb6e2c7bf27757dd1ea5e57ea3d5fab9ba87
CCHIVE <cc.hive+git@outlook.com>
2024-08-22 23:36:31 +0900

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -5,6 +5,9 @@
 # ls /sys/firmware/efi
 # lsblk | grep -v 'rom\|loop\|airoot'
 
+if [ "x$1" = "-h" -o "x$1" = "--help" ]; then
+  echo "Usage: $0 [-h|--help]"
+fi
 
 if [[ $(ls /sys/firmware/efi | grep 'efivars') == *efivars* ]]; then
   echo "Found a efivars!"
@@ -48,3 +51,34 @@ pacstrap /mnt base base-devel linux linux-firmware grub efibootmgr dosfstools ne
 
 # Genarate fstab
 genfstab -U /mnt >> /mnt/etc/fstab
+
+
+arch-chroot /mnt /bin/bash
+
+# vim /etc/locale.gen
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8 UTF-8" > /etc/locale.conf
+
+# tzselect
+#  > 5、20、1
+ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+hwclock --systohc --utc
+
+echo "ARCH" > /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1 localhost" >> /etc/hosts
+
+mkinitcpio -p linux
+# passwd
+
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --boot-directory=/boot/efi/EFI --recheck
+grub-mkconfig -o /boot/efi/EFI/grub/grub.cfg
+
+# Fallback bootloader
+# EFI\boot\bootx64.efi
+mkdir -p /boot/efi/EFI\boot\
+cp /boot/efi/EFI/grub/grubx64.efi /boot/efi/EFI\boot\bootx64.efi
+
+exit
+exit 1
