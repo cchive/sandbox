0c0fe46dcfbbc7e9393f98cb9a4258f02bebd802
CCHIVE <cc.hive+git@outlook.com>
2024-08-22 22:27:00 +0900

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -16,21 +16,21 @@ lsblk | grep -v 'rom\|loop\|airoot'
 # exit 1
 
 DEVICE=/dev/sda
-DEVICE_1_EFI_=$(DEVICE)1
-DEVICE_2_BOOT=$(DEVICE)2
-DEVICE_3_MNT_=$(DEVICE)3
+DEVICE_1_EFI_=${!DEVICE}1
+DEVICE_2_BOOT=${!DEVICE}2
+DEVICE_3_MNT_=${!DEVICE}3
 
-echo sgdisk -z $(DEVICE)
-echo sgdisk -n 1:0:+512M -t 1:ef00 -c 1:"EFI System" $(DEVICE)
-echo sgdisk -n 2:0:+512M -t 2:8300 -c 2:"Linux filesystem"　$(DEVICE)
-echo sgdisk -n 3:0: -t 3:8300 -c 3:"Linux filesystem" $(DEVICE)
+echo sgdisk -z ${!DEVICE}
+echo sgdisk -n 1:0:+512M -t 1:ef00 -c 1:"EFI System" ${!DEVICE}
+echo sgdisk -n 2:0:+512M -t 2:8300 -c 2:"Linux filesystem"　${!DEVICE}
+echo sgdisk -n 3:0: -t 3:8300 -c 3:"Linux filesystem" ${!DEVICE}
 
-echo mkfs.vfat -F32 $(DEVICE_1_EFI_)
-echo mkfs.ext4 $(DEVICE_2_BOOT)
-echo mkfs.ext4 $(DEVICE_3_MNT_)
+echo mkfs.vfat -F32 ${!DEVICE_1_EFI_}
+echo mkfs.ext4 ${!DEVICE_2_BOOT}
+echo mkfs.ext4 ${!DEVICE_3_MNT_}
 
-echo mount $(DEVICE_3_MNT_) /mnt
+echo mount ${!DEVICE_3_MNT_} /mnt
 echo mkdir /mnt/boot
-echo mount $(DEVICE_2_BOOT) /mnt/boot
+echo mount ${!DEVICE_2_BOOT} /mnt/boot
 echo mkdir /mnt/boot/efi
-echo mount $(DEVICE_1_EFI_) /mnt/boot/efi
+echo mount ${!DEVICE_1_EFI_} /mnt/boot/efi
