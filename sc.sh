2cde65cb002a2da331c3a088d9937a622781fb49
CCHIVE <cc.hive+git@outlook.com>
2024-08-23 01:18:00 +0900

    Create sc.sh

---
+++ sc.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# arch-chroot /mnt /bin/bash
+# tzselect
+#  > 5、20、1
+
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8 UTF-8" > /etc/locale.conf
+
+ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+hwclock --systohc --utc
+
+echo "ARCH" > /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1 localhost" >> /etc/hosts
+
+mkinitcpio -p linux
+
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --boot-directory=/boot/efi/EFI --recheck
+grub-mkconfig -o /boot/efi/EFI/grub/grub.cfg
+
+# Fallback bootloader
+# EFI\boot\bootx64.efi
+mkdir -p /boot/efi/EFI\boot\
+cp /boot/efi/EFI/grub/grubx64.efi /boot/efi/EFI\boot\bootx64.efi
+
+exit 0
