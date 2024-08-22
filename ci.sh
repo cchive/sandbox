1654ce357e54b4b319ab75aedf2f997a94d48182
CCHIVE <cc.hive+git@outlook.com>
2024-08-22 22:04:41 +0900

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# curl -o /home/ci.sh https://raw.githubusercontent.com/cchive/sandbox/main/ci.sh
+#chmod 777 /home/ci.sh
+
 ls /sys/firmware/efi
+
+if [[ $(ls /sys/firmware/efi | grep 'efivars') == *efivars* ]]; then
+  echo "Found a efivars!"
+fi
